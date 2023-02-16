5bca7dc364f2b3cca364cf8f855f8d2d66613ac3
Alex Cupit <alex.cupit@MacBook-Pro.local>
2023-02-16 17:04:11 +0000

    feat: adds individual components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts">
+import Banner from './components/Banner.vue'
+import Sidebar from './components/Sidebar.vue'
+import Viewer from './components/Viewer.vue'
 </script>
 
 <template>
-  <div class="banner">
-
-  </div>
+  <div class="banner"><Banner></Banner></div>
 
   <div class="app">
-      <div class="sidebar">
-
+    <div class="sidebar">
+      <Sidebar></Sidebar>
     </div>
 
     <div class="photo">
-
+      <Viewer></Viewer>
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
-
-
-
 .banner {
   background-color: red;
   height: var(--unit);
@@ -44,5 +40,4 @@
   height: calc(100vh - var(--unit));
   width: calc(100vw - calc(2 * var(--unit)));
 }
-
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <RoverSelector></RoverSelector>
+  <DateSelector></DateSelector>
+  <MissionProgress></MissionProgress>
+  <CamSelector></CamSelector>
+  <!-- rover diagram will live here -->
+</template>
+
+<script lang="ts" setup>
+import CamSelector from './CamSelector.vue';
+import DateSelector from './DateSelector.vue';
+import RoverSelector from './RoverSelector.vue';
+import MissionProgress from './MissionProgress.vue';
+</script>
+
+<style scoped></style>
