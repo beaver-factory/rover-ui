5680a0214c659fdcc700009e18aa9ac2cd38d18f
xav <xavier.bruggen@r2-factory.com>
2023-02-16 14:39:27 +0000

    refactor: add correct typing to store

--- src/stores/roverStore.ts
+++ src/stores/roverStore.ts
@@ -1,8 +1,32 @@
 import {defineStore} from 'pinia'
 import {ref} from 'vue'
 
+interface Camera {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+}
+
+interface Rover {
+    id: number;
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+}
+
+interface RoverPhoto {
+    id: number;
+    sol: number;
+    camera: Camera;
+    img_src: string;
+    earth_date: string;
+    rover: Rover;
+}
+
 export const useRoverStore = defineStore('rover', () => {
-    const photos = ref<string[]>([]);
+    const photos = ref<RoverPhoto[]>([]);
 
     return {photos};
 })
