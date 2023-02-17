7436d5b48d7f8a3aa8a4c0b599dd9b873b0024b3
Matt Scarth-Saunders <matt.scarthsaunders@r2-factory.com>
2023-02-17 11:56:15 +0000

    feat: adding rover selector

--- src/components/RoverSelector.vue
+++ src/components/RoverSelector.vue
@@ -1,27 +1,57 @@
 <template>
-  <div>
-    <select @change="handleSelect">
-      <option>Curiosity</option>
-      <option>Opportunity</option>
-      <option>Spirit</option>
+  <BaseUnit
+    :height="1"
+    :width="2"
+    :padding-units="1.5"
+    :border-bottom="true"
+    :border-right="true"
+  >
+    <label class="overlapping-label" for="rover-select">Rover</label>
+    <select
+      id="roverSelect"
+      v-model="formStore.selectedRover"
+      @change="handleSelect"
+    >
+      <option value="curiosity">Curiosity</option>
+      <option value="opportunity">Opportunity</option>
+      <option value="spirit">Spirit</option>
     </select>
-  </div>
+  </BaseUnit>
 </template>
 
 <script lang="ts" setup>
 import { useFormStore } from '../stores/formStore'
 import { useRoverStore } from '../stores/roverStore'
+import BaseUnit from './BaseUnit.vue'
 
 const roverStore = useRoverStore()
 const formStore = useFormStore()
 
-const handleSelect = (e: Event): void => {
-  const element = e.target as HTMLInputElement
-  const rover = element.value.toLowerCase()
+formStore.selectedRover = 'curiosity'
 
-  formStore.selectedRover = rover
-  roverStore.setManifest(rover)
+const handleSelect = (): void => {
+  roverStore.setManifest(formStore.selectedRover)
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+select {
+  /* width: min-content; */
+  width: 100%;
+  padding: 0.5rem;
+  background-color: #004f72;
+  color: white;
+  border: 1px solid white;
+  font-size: 1rem;
+}
+
+.overlapping-label {
+  position: absolute;
+  font-size: 0.7rem;
+  margin-top: -0.45rem;
+  margin-left: 0.3rem;
+  background-color: #004f72;
+  color: white;
+  padding: 0 0.5rem;
+}
+</style>
