b20cc5a637bc3906d6ba60c800c5d6eb2dde03a2
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-02-17 11:35:31 +0000

    chore: add config and dependencies for eslint airbnb-style with typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+  ignorePatterns: ['vite.config.ts'],
+}
