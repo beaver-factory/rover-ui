ee5f6708123f4ab5c9add8eaa680c2876952d182
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-02-17 15:40:55 +0000

    feat: add basic mission info component

---
+++ src/components/MissionInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseUnit :width="2" :border-right="true" :border-bottom="true">
+    <p v-if="roverStore.manifest.name === 'Curiosity'">
+      {{ roverStore.manifest.name }} has been active for
+      {{ roverStore.manifest.max_sol }} solar days on Mars. During this time,
+      the rover has taken a total of
+      {{ roverStore.manifest.total_photos }} photos.
+    </p>
+    <p v-else>
+      {{ roverStore.manifest.name }} was active for a total of
+      {{ roverStore.manifest.max_sol }} solar days on Mars. During this time,
+      the rover took a total of {{ roverStore.manifest.total_photos }} photos.
+    </p>
+  </BaseUnit>
+</template>
+
+<script lang="ts" setup>
+import BaseUnit from './BaseUnit.vue'
+import { useRoverStore } from '../stores/roverStore'
+
+const roverStore = useRoverStore()
+</script>
+
+<style scoped>
+p {
+  color: white;
+  font-size: 0.6rem;
+}
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,6 @@
 <template>
   <RoverSelector></RoverSelector>
+  <MissionInfo></MissionInfo>
   <DateSelector></DateSelector>
   <MissionProgress></MissionProgress>
   <CamSelector></CamSelector>
@@ -7,10 +8,11 @@
 </template>
 
 <script lang="ts" setup>
-import CamSelector from './CamSelector.vue';
-import DateSelector from './DateSelector.vue';
-import RoverSelector from './RoverSelector.vue';
-import MissionProgress from './MissionProgress.vue';
+import CamSelector from './CamSelector.vue'
+import DateSelector from './DateSelector.vue'
+import RoverSelector from './RoverSelector.vue'
+import MissionProgress from './MissionProgress.vue'
+import MissionInfo from './MissionInfo.vue'
 </script>
 
 <style scoped></style>
