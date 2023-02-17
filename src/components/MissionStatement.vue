ce4109c8ab1d2668326105d150cfb141bf9694e7
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-02-17 14:31:54 +0000

    style: add base unit styling for text container

--- src/components/MissionStatement.vue
+++ src/components/MissionStatement.vue
@@ -1,7 +1,11 @@
 <template>
-  <div></div>
+  <BaseUnit :width="2" :border-right="true" :border-bottom="true">
+    <p>text goes here</p></BaseUnit
+  >
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import BaseUnit from './BaseUnit.vue'
+</script>
 
 <style scoped></style>
