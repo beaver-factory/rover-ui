0d4e0aa2355cb447609a0efe213dc42eb62cb604
Chris-Duignan <cduignan.1993@gmail.com>
2023-02-20 14:05:01 +0000

    refactor: refactor sidebar tag to be self closing

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import Viewer from './components/Viewer.vue'
 
   <div class="app">
     <div class="sidebar">
-      <Sidebar></Sidebar>
+      <Sidebar />
     </div>
 
     <Viewer />
@@ -25,11 +25,5 @@ import Viewer from './components/Viewer.vue'
   background-color: blue;
   height: calc(100vh - var(--unit));
   width: calc(2 * var(--unit));
-}
-
-.photo {
-  background-color: green;
-  height: calc(100vh - var(--unit));
-  width: calc(100vw - calc(2 * var(--unit)));
 }
 </style>
