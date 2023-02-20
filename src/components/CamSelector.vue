5c0bb371a3b91e47273e8781fdd97673ef9b1b7a
Alex Cupit <alex.cupit@MacBook-Pro.local>
2023-02-20 11:50:24 +0000

    feat: cam selector start

--- src/components/CamSelector.vue
+++ src/components/CamSelector.vue
@@ -1,9 +1,79 @@
 <template>
-  <BaseUnit :width="2" :height="1" :border-right="true" :border-bottom="true" />
+  <BaseUnit :height="1" :width="2" :border-bottom="true" :border-right="true">
+    <div class="selectContainer">
+      <label class="overlapping-label" for="camera-select">Camera</label>
+      <select
+        id="camera-select"
+        v-model="formStore.selectedCam"
+        @change="handleSelect"
+      >
+        <option value="">All</option>
+        <option v-for="cam in availableCams">{{ cam }}</option>
+      </select>
+    </div>
+    {{ availableCams }}
+  </BaseUnit>
 </template>
 
 <script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { watch, ref } from 'vue'
+import { useFormStore } from '../stores/formStore'
+import { useRoverStore } from '../stores/roverStore'
+
 import BaseUnit from './BaseUnit.vue'
+
+const roverStore = useRoverStore()
+const formStore = useFormStore()
+
+roverStore.setManifest('curiosity')
+formStore.selectedDate = '2023-2-3'
+
+const { manifest } = storeToRefs(roverStore)
+const { selectedRover, selectedDate, selectedCam } = storeToRefs(formStore)
+const availableCams = ref<string[]>([])
+
+const findCams = () => {
+  manifest.value.photos.filter((day) => {
+    day.earth_date === formStore.selectedDate
+    availableCams.value = day.cameras
+  })
+}
+findCams()
+
+watch([selectedRover, selectedDate], () => {
+  findCams()
+})
+
+const handleSelect = (): void => {
+  roverStore.setPhotos(selectedRover, selectedDate, selectedCam)
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+select {
+  width: 100%;
+  padding: 0.7rem;
+  font-size: 1rem;
+  color: white;
+  background-color: #004f72;
+  border: 1px solid white;
+}
+
+.selectContainer {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.overlapping-label {
+  position: absolute;
+  font-size: 0.6rem;
+  margin-top: -2.8rem;
+  margin-left: 0.3rem;
+  padding: 0 0.3rem;
+  background-color: #004f72;
+  color: white;
+}
+</style>
