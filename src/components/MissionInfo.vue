398eec17a8bb5a02423b3d763b1cb6bdf3eb8195
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-02-20 11:28:54 +0000

    refactor: make html more dry

--- src/components/MissionInfo.vue
+++ src/components/MissionInfo.vue
@@ -1,16 +1,14 @@
 <template>
   <BaseUnit :width="2" :border-right="true" :border-bottom="true">
-    <div>
-      <p v-if="roverStore.manifest.name === 'Curiosity'">
-        <strong>{{ roverStore.manifest.name }}</strong> has been active for
+    <div v-if="!roverStore.manifest_loading">
+      <p>
+        <strong>{{ roverStore.manifest.name }}</strong>
+        <span v-if="roverStore.manifest.name === 'Curiosity'"> has been </span
+        ><span v-else> was </span>active for
         <strong>{{ roverStore.manifest.max_sol }}</strong> solar days on Mars.
-        During this time, the rover has taken a total of
-        <strong>{{ roverStore.manifest.total_photos }}</strong> photos.
-      </p>
-      <p v-else>
-        <strong>{{ roverStore.manifest.name }}</strong> was active for a total
-        of <strong>{{ roverStore.manifest.max_sol }}</strong> solar days on
-        Mars. During this time, the rover took a total of
+        During this time, the rover
+        <span v-if="roverStore.manifest.name === 'Curiosity'"> has taken </span
+        ><span v-else> took </span> a total of
         <strong>{{ roverStore.manifest.total_photos }}</strong> photos.
       </p>
     </div>
