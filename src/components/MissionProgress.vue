ace00e11052b675eb3bb70261bbf380b70c10461
Matt Scarth-Saunders <matt.scarthsaunders@r2-factory.com>
2023-02-20 14:14:17 +0000

    feat: basis for progress bar implemented

--- src/components/MissionProgress.vue
+++ src/components/MissionProgress.vue
@@ -1,7 +1,66 @@
 <template>
-  <div></div>
+  <BaseUnit :height="1" :width="2" :border-bottom="true" :border-right="true">
+    <div class="progressContainer">
+      <label for="missionProgressBar">Mission Progress</label>
+      <progress
+        id="missionProgressBar"
+        :value="missionProgress"
+        max="100"
+      ></progress>
+    </div>
+  </BaseUnit>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import BaseUnit from './BaseUnit.vue'
 
-<style scoped></style>
+const missionProgress = ref<number>(5)
+</script>
+
+<style scoped>
+.progressContainer {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+::-webkit-progress-bar {
+  width: 100%;
+  height: 20px;
+  border-radius: 0;
+  border: 1px solid white;
+  background-color: #004f72;
+}
+
+/* Firefox */
+progress::-moz-progress-bar {
+  width: 100%;
+  height: 20px;
+  border-radius: 0;
+  border: 1px solid white;
+  background: white;
+}
+::-moz-progress-bar {
+  width: 100%;
+  height: 20px;
+  border-radius: 0;
+  border: 1px solid white;
+  background-color: #004f72;
+}
+
+/* Chrome/Safari */
+progress::-webkit-progress-value {
+  background: white;
+}
+
+/* IE10 */
+progress {
+  background-color: #004f72;
+}
+
+progress {
+  color: white;
+}
+</style>
