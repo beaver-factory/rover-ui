400b3ecb16b33e1113cbb360af8e28461477ef4d
Alex Cupit <alex.cupit@MacBook-Pro.local>
2023-02-21 16:11:30 +0000

    feat: uses select component for rover selector

--- src/components/RoverSelector.vue
+++ src/components/RoverSelector.vue
@@ -1,61 +1,31 @@
 <template>
-  <BaseUnit :height="1" :width="2" :border-bottom="true" :border-right="true">
-    <div class="selectContainer">
-      <label class="overlapping-label" for="rover-select">Rover</label>
-      <select
-        id="roverSelect"
-        v-model="formStore.selectedRover"
-        @change="handleSelect"
-      >
-        <option value="curiosity">Curiosity</option>
-        <option value="opportunity">Opportunity</option>
-        <option value="spirit">Spirit</option>
-      </select>
-    </div>
-  </BaseUnit>
+  <SelectComponent
+    :options="rovers"
+    id="rover-select"
+    label="Rover"
+    @selected="(selected) => handleSelect(selected)"
+  ></SelectComponent>
 </template>
 
 <script lang="ts" setup>
 import useFormStore from '../stores/formStore'
 import useRoverStore from '../stores/roverStore'
 
-import BaseUnit from './BaseUnit.vue'
+import SelectComponent from './SelectComponent.vue'
 
 const roverStore = useRoverStore()
 const formStore = useFormStore()
 
+const rovers = ['Curiosity', 'Opportunity', 'Spirit']
+
 // setting default rover and initial manifest API call
 roverStore.setManifest(formStore.selectedRover)
 
-const handleSelect = (): void => {
+const handleSelect = (selected: string): void => {
+  formStore.selectedRover = selected
+  console.log(formStore.selectedRover)
   roverStore.setManifest(formStore.selectedRover)
 }
 </script>
 
-<style scoped>
-select {
-  width: 100%;
-  padding: 0.7rem;
-  font-size: 1rem;
-  color: white;
-  background-color: #004f72;
-  border: 1px solid white;
-}
-
-.selectContainer {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.overlapping-label {
-  position: absolute;
-  font-size: 0.6rem;
-  margin-top: -2.8rem;
-  margin-left: 0.3rem;
-  padding: 0 0.3rem;
-  background-color: #004f72;
-  color: white;
-}
-</style>
+<style scoped></style>
