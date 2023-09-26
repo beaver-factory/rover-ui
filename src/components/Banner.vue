f36745a258d93356c58be9879ba7e9117ea61543
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-09-26 18:27:45 +0100

    feat: responsive text size for rover title, change font

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,7 +2,7 @@
   <BaseUnit :width="'100vw'" :border-bottom="true">
     <div class="logo-container">
       <!-- <img class="banner-logo" src="../assets/r2-logo.png" /> -->
-      <h1 class="banner-title">Rover</h1>
+      <h1 class="banner-title">ROVER</h1>
     </div>
   </BaseUnit>
 </template>
@@ -12,6 +12,7 @@ import BaseUnit from './BaseUnit.vue'
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Jura:wght@500&display=swap');
 .logo-container {
   height: 100%;
   display: flex;
@@ -23,12 +24,15 @@ import BaseUnit from './BaseUnit.vue'
 }
 
 .banner-title {
+  width: calc((2 * var(--unit)) - (2 * var(--padding)));
   color: white;
-  letter-spacing: 0.5rem;
-  font-size: 3rem;
+  letter-spacing: calc(((2 * var(--unit)) - (2 * var(--padding))) / 11);
+  font-size: calc(((2 * var(--unit)) - (2 * var(--padding))) / 5);
   background-image: linear-gradient(white, white);
   background-size: 100% 3px;
   background-repeat: no-repeat;
   background-position: 100% 0%;
+  font-family: 'Jura', Courier, monospace;
+  text-align: center;
 }
 </style>
